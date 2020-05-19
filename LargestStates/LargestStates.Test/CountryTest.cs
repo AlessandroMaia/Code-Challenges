77a0bbcb9de03e643fe3c70faee50e66bed2af1c
AlessandroMaia <ale.maia.alm@gmail.com>
2020-05-18 21:31:50 -0300

    Insert class CountryTest

---
+++ LargestStates/LargestStates.Test/CountryTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace LargestStates.Test
+{
+    public class CountryTest
+    {
+        [Fact]
+        public void Should_Return_10_Itens_When_Get_Top_10_States()
+        {
+            var states = new Country();
+            var top = states.Top10StatesByArea();
+            Assert.NotNull(top);
+            Assert.Equal(10, top.Length);
+        }
+    }
+}
