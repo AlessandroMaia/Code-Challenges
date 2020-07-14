a54bb99736341e3968a6f109233652fb83867c27
AlessandroMaia <ale.maia.alm@gmail.com>
2020-07-14 19:37:16 -0300

    Creating company map

---
+++ CreatingEntities/Source/Models/CompanyMap.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Source.Models
+{
+    public class CompanyMap : IEntityTypeConfiguration<Company>
+    {
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+            builder
+                .ToTable("company");
+
+            builder
+                .HasKey(x => x.Id)
+                .HasName("id");
+
+            builder
+                .Property(x => x.Name)
+                .HasColumnName("name")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder
+                .Property(x => x.Slug)
+                .HasColumnName("slug")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder
+                .Property(x => x.CreatedAt)
+                .HasColumnName("created_at")
+                .IsRequired();
+
+            builder
+                .HasMany(x => x.Candidates)
+                .WithOne(x => x.Company)
+                .HasForeignKey(x => x.CompanyId)
+                .IsRequired();
+
+        }
+    }
+}
