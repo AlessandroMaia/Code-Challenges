6042ebe8d43e424ff1aa7a58df38b4df644de85e
AlessandroMaia <ale.maia.alm@gmail.com>
2020-07-14 19:35:11 -0300

    Creating candidate map

---
+++ CreatingEntities/Source/Models/CandidateMap.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Source.Models
+{
+    public class CandidateMap : IEntityTypeConfiguration<Candidate>
+    {
+        public void Configure(EntityTypeBuilder<Candidate> builder)
+        {
+            builder.ToTable("candidate");
+
+            builder
+                .HasKey(x => new { x.UserId, x.AccelerationId, x.CompanyId });
+
+            builder
+                .Property(x => x.UserId)
+                .HasColumnName("user_id")
+                .IsRequired();
+
+            builder
+                .Property(x => x.AccelerationId)
+                .HasColumnName("acceleration_id")
+                .IsRequired();
+
+            builder
+                .Property(x => x.CompanyId)
+                .HasColumnName("company_id")
+                .IsRequired();
+
+            builder
+                .Property(x => x.Status)
+                .HasColumnName("status")
+                .IsRequired();
+
+            builder
+                .Property(x => x.CreatedAt)
+                .HasColumnName("created_at")
+                .IsRequired();
+        }
+    }
+}
