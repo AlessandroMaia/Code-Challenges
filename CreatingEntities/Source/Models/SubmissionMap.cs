2f7b4954e1df5fde388791e84f4194cb0bc4c207
AlessandroMaia <ale.maia.alm@gmail.com>
2020-07-14 19:37:53 -0300

    Creating submission map

---
+++ CreatingEntities/Source/Models/SubmissionMap.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Source.Models
+{
+    public class SubmissionMap : IEntityTypeConfiguration<Submission>
+    {
+        public void Configure(EntityTypeBuilder<Submission> builder)
+        {
+            builder.ToTable("submission");
+
+            builder
+                .HasKey(x => new { x.UserId, x.ChallengeId });
+
+            builder
+                .Property(x => x.UserId)
+                .HasColumnName("user_id")
+                .IsRequired();
+
+            builder
+                .Property(x => x.ChallengeId)
+                .HasColumnName("challenge_id")
+                .IsRequired();
+
+            builder
+                .Property(x => x.Score)
+                .HasColumnName("score")
+                .IsRequired();
+
+            builder
+                .Property(x => x.CreatedAt)
+                .HasColumnName("created_at")
+                .IsRequired(); 
+        }
+    }
+}
